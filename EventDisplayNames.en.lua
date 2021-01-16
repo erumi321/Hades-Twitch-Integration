1a6ddfcf8e88379dee34e4aa64c09d6a3520ad2b
erumi321 <42208651+erumi321@users.noreply.github.com>
2021-01-16 10:55:33 -0500

    Changed sjson to use lua
    Changed sjson to use lua to be able to print to Twitch chat easier

---
+++ EventDisplayNames.en.lua
@@ -0,0 +1,104 @@
+EventDisplayNames = {
+ ["TwitchIntegration_PLAYER_HEAL"] =  {
+    DisplayName = "Heal Zagreus"
+  },
+  ["TwitchIntegration_PLAYER_DAMAGE"] = {
+    DisplayName = "Damage Zagreus"
+  },
+ ["TwitchIntegration_SPAWN_CENTAUR"] =  {
+    DisplayName = "Spawn Centaur Heart"
+  },
+  ["TwitchIntegration_SPAWN_HAMMER"] = {
+    DisplayName = "Spawn Hammer"
+  },
+  ["TwitchIntegration_SPAWN_POM"] =  {
+    DisplayName = "Spawn Pom"
+  },
+  ["TwitchIntegration_REMOVE_RANDOM_BOON"] = {
+    DisplayName = "Remove a boon"
+  },
+  ["TwitchIntegration_SPAWN_RANDOM_BOON"] = {
+    DisplayName = "Spawn a boon"
+  },
+  ["TwitchIntegration_SPAWN_RANDOM_ENEMIES"] = {
+    DisplayName = "Spawn enemies"
+  },
+  ["TwitchIntegration_GIVE_MONEY"] = {
+    DisplayName = "Give money"
+  },
+  ["TwitchIntegration_REMOVE_MONEY"] = {
+    DisplayName = "Remove money"
+  },
+  ["TwitchIntegration_EMPTY_SUPER_METER"] = {
+    DisplayName = "Drain Aid meter"
+  },
+  ["TwitchIntegration_FILL_SUPER_METER"] = {
+    DisplayName = "Fill Aid meter"
+  },
+  ["TwitchIntegration_DISABLE_DASH"] = {
+    DisplayName = "Disable dash"
+  },
+  ["TwitchIntegration_ROOM_WIPE"] = {
+    DisplayName = "Kill all enemies"
+  },
+  ["TwitchIntegration_GIVE_DARKNESS"] = {
+    DisplayName = "Give darkness"
+  },
+  ["TwitchIntegration_GIVE_NECTAR"] = {
+    DisplayName = "Give nectar"
+  },
+  ["TwitchIntegration_GIVE_KEY"] = {
+    DisplayName = "Give key"
+  },
+  ["TwitchIntegration_GIVE_BLOOD"] = {
+    DisplayName = "Give titan blood"
+  },
+  ["TwitchIntegration_GIVE_GEMS"] = {
+    DisplayName = "Give gems"
+  },
+  ["TwitchIntegration_GIVE_DIAMOND"] = {
+    DisplayName = "Give diamond"
+  },
+  ["TwitchIntegration_CHARM_ALL_ENEMIES"] = {
+    DisplayName = "Charm all enemies"
+  },
+  ["TwitchIntegration_SPAWN_MINI_CHARIOTS"] = {
+    DisplayName = "Spawn mini chariots"
+  },
+  ["TwitchIntegration_SPAWN_PESTS"] = {
+    DisplayName = "Spawn pests"
+  },
+  ["TwitchIntegration_SPAWN_CHAOS_BOON"] = {
+    DisplayName = "Spawn Chaos boon"
+  },
+  ["TwitchIntegration_HIDE_UI"] = {
+    DisplayName = "Hide interface"
+  },
+  ["TwitchIntegration_GIVE_TEMP_RARITY_BOOST"] = {
+    DisplayName = "Next boon rarity increased"
+  },
+  ["TwitchIntegration_FORCE_CHAOS_GATE"] = {
+    DisplayName = "Force Chaos gate"
+  },
+  ["TwitchIntegration_FORCE_TROVE"] = {
+    DisplayName = "Force infernal trove"
+  },
+  ["TwitchIntegration_FORCE_FISH"] = {
+    DisplayName = "Force fishing point"
+  },
+  ["TwitchIntegration_ADD_DEFIANCE"] = {
+    DisplayName = "Give Death Defiance"
+  },
+  ["TwitchIntegration_BOSS_MINELAYER"] = {
+    DisplayName = "Minelayer boss battle"
+  },
+  ["TwitchIntegration_BOSS_RAT_THUG"] = {
+    DisplayName = "Rat thug boss battle"
+  },
+  ["TwitchIntegration_BOSS_HELPING_HANDS"] = {
+    DisplayName = "Hands boss battle"
+  },
+  ["TwitchIntegration_CREATE_DEVOTION_EFFECT"] = {
+    DisplayName = "Anger a random god"
+  },
+}
